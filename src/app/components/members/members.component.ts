ee49e91df1e4fd4b139c044e7a0b486bc7fce34e
Jaime Torres <jaime.torres@version1.com>
2023-10-22 18:34:25 +0200

    members component updated

--- src/app/components/members/members.component.ts
+++ src/app/components/members/members.component.ts
@@ -7,6 +7,7 @@ import {MemberService} from "../../services/member.service";
 import {MatTableDataSource} from "@angular/material/table";
 import {MovementType} from "../../entities/MovementType";
 import {Role} from "../../entities/Role";
+import {BreakpointObserver} from "@angular/cdk/layout";
 
 @Component({
   selector: 'app-members',
@@ -16,16 +17,39 @@ import {Role} from "../../entities/Role";
 export class MembersComponent implements OnInit {
   isLoading$ = new BehaviorSubject(true);
   dataSource = new MatTableDataSource<Member>();
-  columnsToDisplay = ['name', 'surname', 'phone', 'nCaptaincies', 'role', 'balance'];
-  columnsToDisplayInSmallScreens = ['createdOn', 'description', 'amount'];
+  columnsToDisplay = ['name', 'surname', 'phone', 'nCaptaincies', 'role', 'balance', 'actions'];
+  columnsToDisplayInSmallScreens = ['name', 'surname', 'actions'];
   columns = this.columnsToDisplay;
+  isAdmin = new BehaviorSubject(false);
 
   constructor(private memberService: MemberService,
               private authService: AuthService,
-              private router: Router) {
+              private router: Router,
+              private responsive: BreakpointObserver) {
   }
 
   ngOnInit(): void {
+    this.authService.member$.subscribe({
+      next: member => {
+        if(member?.role == Role.ADMIN){
+          this.isAdmin.next(true);
+        }
+      }
+    });
+    this.responsive.observe(['(min-width: 700px)']).subscribe(
+      {
+        next: value => {
+          if (value.matches) {
+            this.columns = this.columnsToDisplay;
+          } else {
+            this.columns = this.columnsToDisplayInSmallScreens;
+          }
+        }
+      }
+    );
+  }
+
+  private getMembers(){
     this.memberService.getAll$().subscribe({
       next: members => {
         this.dataSource.data = members;
@@ -35,7 +59,7 @@ export class MembersComponent implements OnInit {
         this.authService.logout();
         this.router.navigateByUrl('/login')
       }
-    })
+    });
   }
 
   protected readonly MovementType = MovementType;
