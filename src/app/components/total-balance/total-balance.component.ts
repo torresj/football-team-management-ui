006475dfb12616747ada2c058459023b444665ad
Jaime Torres <jtbenavente@gmail.com>
2023-12-01 15:12:30 +0100

    Total balance updated

--- src/app/components/total-balance/total-balance.component.ts
+++ src/app/components/total-balance/total-balance.component.ts
@@ -1,7 +1,7 @@
-import {Component, OnInit} from '@angular/core';
-import {BehaviorSubject} from "rxjs";
-import {MovementService} from "../../services/movement.service";
-import {MovementType} from "../../entities/MovementType";
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { MovementService } from '../../services/movement.service';
+import { MovementType } from '../../entities/MovementType';
 
 export interface TableData {
   name: string;
@@ -11,10 +11,9 @@ export interface TableData {
 @Component({
   selector: 'app-total-balance',
   templateUrl: './total-balance.component.html',
-  styleUrls: ['./total-balance.component.css']
+  styleUrls: ['./total-balance.component.css'],
 })
 export class TotalBalanceComponent implements OnInit {
-
   isLoading$ = new BehaviorSubject(true);
   columns: string[] = ['name', 'amount'];
   dataSource: TableData[] = [];
@@ -24,38 +23,55 @@ export class TotalBalanceComponent implements OnInit {
   fieldAnnualPay = 1500;
   domainAnnualPay = 20;
 
-  constructor(private movementService: MovementService) {
-  }
+  constructor(private movementService: MovementService) {}
 
   ngOnInit(): void {
     this.movementService.getTotalBalance$().subscribe({
-      next: balance => {
+      next: (balance) => {
         this.dataSource = [
-          {name: "Balance 22/23", amount: this.lastYearBalance},
-          {name: "Cuotas y multas totales", amount: balance.totalExpenses * -1},
-          {name: "Cuotas y multas pagadas", amount: balance.totalIncomes},
-          {name: "Cuotas y multas sin pagar", amount: (balance.totalIncomes + balance.totalExpenses) * -1},
-          {name: "Pago del campo", amount: this.fieldAnnualPay},
-          {name: "Pago del dominio web pkmh.es", amount: this.domainAnnualPay},
-          {name: "Compra de nuevo balón", amount: 126}
-        ]
-        this.totalDataSource = [{
-          name: "Ingresos totales (temporada anterior + cuotas y multas pagadas)",
-          amount: this.lastYearBalance + balance.totalIncomes
-        },
+          { name: 'Balance 22/23', amount: this.lastYearBalance },
           {
-            name: "Gastos totales (pago del campo + otros gastos)",
-            amount: this.fieldAnnualPay + this.domainAnnualPay + 126
-          }
+            name: 'Cuotas y multas totales',
+            amount: balance.totalExpenses * -1,
+          },
+          { name: 'Cuotas y multas pagadas', amount: balance.totalIncomes },
+          {
+            name: 'Cuotas y multas sin pagar',
+            amount: balance.totalIncomes + balance.totalExpenses,
+          },
+          { name: 'Pago del campo', amount: this.fieldAnnualPay * -1 },
+          {
+            name: 'Pago del dominio web pkmh.es',
+            amount: this.domainAnnualPay * -1,
+          },
+          { name: 'Compra de nuevo balón', amount: -126 },
+          {
+            name: 'Equipación de repuesto + dos juegos de guantes',
+            amount: -94,
+          },
         ];
-        this.total = -this.fieldAnnualPay + this.lastYearBalance + balance.totalIncomes;
+        this.totalDataSource = [
+          {
+            name: 'Ingresos totales (temporada anterior + cuotas y multas pagadas)',
+            amount: this.lastYearBalance + balance.totalIncomes,
+          },
+          {
+            name: 'Gastos totales (pago del campo + otros gastos)',
+            amount:
+              (this.fieldAnnualPay + this.domainAnnualPay + 126 + 94) * -1,
+          },
+        ];
+        this.total =
+          this.lastYearBalance +
+          balance.totalIncomes -
+          (this.fieldAnnualPay + this.domainAnnualPay + 126 + 94);
         this.isLoading$.next(false);
       },
-      error: err => {
+      error: (err) => {
         console.log(err);
         this.isLoading$.next(false);
-      }
-    })
+      },
+    });
   }
 
   protected readonly MovementType = MovementType;
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,55 +1,70 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {apiConstants} from "../constants/api.constants";
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { apiConstants } from '../constants/api.constants';
 import * as moment from 'moment';
-import {BehaviorSubject, concatWith, shareReplay, tap} from "rxjs";
-import Jwt from "../entities/Jwt";
-import {MemberService} from "./member.service";
-import Member from "../entities/Member";
-import {Router} from "@angular/router";
-import {Role} from "../entities/Role";
+import { BehaviorSubject, concatWith, shareReplay, tap } from 'rxjs';
+import Jwt from '../entities/Jwt';
+import { MemberService } from './member.service';
+import Member from '../entities/Member';
+import { Router } from '@angular/router';
+import { Role } from '../entities/Role';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  private jwt_key = "JWT_KEY";
+  private jwt_key = 'JWT_KEY';
   member$ = new BehaviorSubject<Member | null>(null);
 
-  constructor(private http: HttpClient, private memberService: MemberService, private router: Router) {
-    if (localStorage.getItem(this.jwt_key) !== null && localStorage.getItem(this.jwt_key) !== '') {
+  constructor(
+    private http: HttpClient,
+    private memberService: MemberService,
+    private router: Router
+  ) {
+    if (
+      localStorage.getItem(this.jwt_key) !== null &&
+      localStorage.getItem(this.jwt_key) !== ''
+    ) {
       this.memberService.getMe$().subscribe({
-        next: value => this.member$.next(value), error: err => {
+        next: (value) => this.member$.next(value),
+        error: (err) => {
           this.logout();
           this.router.navigateByUrl('/login');
-        }
-      })
+        },
+      });
     }
   }
 
   login$(username: string, password: string) {
-    const nonce = moment().unix()
-    return this.http.post<Jwt>(apiConstants.apiBaseUrl + '/v1/login', {
-      username,
-      password,
-      nonce
-    }).pipe(
-      shareReplay(1),
-      tap(jwt => {
-        localStorage.setItem(this.jwt_key, jwt.jwt);
-        console.log(jwt.jwt)
-      }),
-      concatWith(this.memberService.getMe$().pipe(tap(member => this.member$.next(member))))
-    );
+    const nonce = moment().unix();
+    return this.http
+      .post<Jwt>(apiConstants.apiBaseUrl + '/v1/login', {
+        username,
+        password,
+        nonce,
+      })
+      .pipe(
+        shareReplay(1),
+        tap((jwt) => {
+          localStorage.setItem(this.jwt_key, jwt.jwt);
+        }),
+        concatWith(
+          this.memberService
+            .getMe$()
+            .pipe(tap((member) => this.member$.next(member)))
+        )
+      );
   }
 
   isAuthenticated() {
-    return localStorage.getItem(this.jwt_key) !== null && localStorage.getItem(this.jwt_key) !== '';
+    return (
+      localStorage.getItem(this.jwt_key) !== null &&
+      localStorage.getItem(this.jwt_key) !== ''
+    );
   }
 
   logout() {
-    localStorage.clear()
+    localStorage.clear();
     this.member$.next(null);
   }
 
