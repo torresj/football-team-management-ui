184eca911df0fed8fab3c6d6e3290cc20b5ff8a5
Jaime Torres <jtbenavente@gmail.com>
2024-01-31 14:26:49 +0100

    Locale changed to es in datepicker

--- src/app/components/dialogs/create-match/create-match.component.ts
+++ src/app/components/dialogs/create-match/create-match.component.ts
@@ -1,33 +1,36 @@
-import {Component} from '@angular/core';
-import {MatDialogRef} from "@angular/material/dialog";
-import {MatchService} from "../../../services/match.service";
-import {AuthService} from "../../../services/auth.service";
-import {Router} from "@angular/router";
+import { Component } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatchService } from '../../../services/match.service';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
+import { DateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-create-match',
   templateUrl: './create-match.component.html',
-  styleUrls: ['./create-match.component.css']
+  styleUrls: ['./create-match.component.css'],
 })
 export class CreateMatchComponent {
-
   dateSelected = new Date();
 
-  constructor(private authService: AuthService,
-              private router: Router,
-              private matchService: MatchService,
-              private dialogRef: MatDialogRef<CreateMatchComponent>) {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private matchService: MatchService,
+    private dialogRef: MatDialogRef<CreateMatchComponent>,
+    private _adapter: DateAdapter<any>
+  ) {
+    this._adapter.setLocale('es');
   }
 
   createMatch() {
-    this.matchService.createMatch$(this.dateSelected)
-      .subscribe({
-        next: () => this.dialogRef.close(),
-        error: err => {
-          this.authService.logout();
-          this.dialogRef.close()
-          this.router.navigateByUrl('/login')
-        }
-      })
+    this.matchService.createMatch$(this.dateSelected).subscribe({
+      next: () => this.dialogRef.close(),
+      error: (err) => {
+        this.authService.logout();
+        this.dialogRef.close();
+        this.router.navigateByUrl('/login');
+      },
+    });
   }
 }
